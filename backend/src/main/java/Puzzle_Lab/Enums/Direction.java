469f06556b28333462c645ba1b143aa59b3b9846
Dhruv Arora <63711315+DhruvArora02@users.noreply.github.com>
2025-11-19 20:01:05 -0600

    Add files via upload

---
+++ backend/src/main/java/Puzzle_Lab/Enums/Direction.java
@@ -0,0 +1,10 @@
+package Puzzle_Lab.Enums;
+
+/**
+ * Used to represent the direction a hint is pointing to. For example, there
+ * could be two words, each starting from the same cell and thus having the same
+ * label. But one hint would be for the word going DOWN, and the other ACROSS.
+ */
+public enum Direction {
+    ACROSS, DOWN
+}
--- backend/src/main/java/Puzzle_Lab/Enums/Status.java
+++ backend/src/main/java/Puzzle_Lab/Enums/Status.java
@@ -3,6 +3,6 @@ package Puzzle_Lab.Enums;
 /**
  * Represents the type of theme the User wants as their main preference.
  */
-public enum Theme {
-    DEFAULT, DARK, RASPBERRY, LAKE, MANGO, BADGER
+public enum Status {
+    PRIVATE, PUBLIC, UNLISTED, DRAFT_CREATE, DRAFT_HINT
 }
